034ca4e6c7383f774c9a9a80669168624554e9ac
Bphreaker <63411948+Bphreaker@users.noreply.github.com>
2023-05-09 10:10:30 +0100

    Update FirebaseMessagingPlugin.java

--- src/android/FirebaseMessagingPlugin.java
+++ src/android/FirebaseMessagingPlugin.java
@@ -134,9 +134,10 @@ public class FirebaseMessagingPlugin extends ReflectiveCordovaPlugin {
     }
 
     @CordovaMethod
-    private void getToken(CordovaArgs args, CallbackContext callbackContext) throws Exception {
-        String type = args.getString(0);
-        if (!type.isEmpty()) {
+    private void getToken(String type, CallbackContext callbackContext) throws Exception {
+        //String type = args.getString(0);
+        //if (!type.isEmpty()) {
+	if (type.isEmpty()) {
             callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, (String) null));
         } else {
             String fcmToken = await(firebaseMessaging.getToken());
